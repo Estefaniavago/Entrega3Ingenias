d4b173c512b57eae99ce8b20f385a354248f4407
Daniela8594 <fannydavidmdz12@gmail.com>
2024-08-05 18:49:50 -0300

    ...

---
+++ database/conect_mysql_db.js
@@ -0,0 +1,24 @@
+
+const { Sequelize } = require('sequelize');
+const { database } = require('../config/config');
+
+// ENVIRONMENT
+const dotenv = require('dotenv');
+dotenv.config()
+
+    const sequelize = new Sequelize (database.DB_NAME, database.DB_USER , database.DB_PASS, {
+    host: database.DB_HOST,
+    dialect: database.dialect 
+    });
+
+    async function authenticate() {
+      try{
+        await sequelize.authenticate ();
+        console.log ('Conexion exitosa a la BD');
+      } catch(error){
+        console.error('Error al conectarse a la BD:' , error);
+      }
+      
+    }
+
+    authenticate()
